9274560641073e70a261ab8b0e08bf8af541f960
carter <carter@LinuxIsBetter>
2018-08-11 17:34:35 -0600

    Added Basic Game Loop

--- src/colorRunner/Program.java
+++ src/colorRunner/Program.java
@@ -1,23 +1,12 @@
 package colorRunner;
 
 import java.awt.Graphics;
-
 import javax.swing.JFrame;
 
-public class Program {
-	
-	Graphics g;
-	
-	public Program() {
-		JFrame game = new JFrame("Color Runner");
-		game.setLayout(null);
-		game.setBounds(400,400,1024,576);
-		game.setVisible(true);
-		g = game.getGraphics();
-		game.paint(g);
-	}
-	
+public class Program extends Thread{	
+	public Program() {}
+
 	public static void main(String[]args) {
-		Program d = new Program();
+		new Game().start();
 	}
 }
