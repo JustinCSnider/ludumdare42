fe5e0cfd0dfaab406707d63999c477a5dca73e9d
carter <carter@LinuxIsBetter>
2018-08-11 17:42:01 -0600

    Fixed Window Scale

--- src/colorRunner/Game.java
+++ src/colorRunner/Game.java
@@ -15,9 +15,9 @@ public class Game extends Canvas implements Runnable{
 	
 	private static final long serialVersionUID = 1L;
 	
-	public static final int WIDTH = 1024;
+	public static final int WIDTH = 200;
 	public static final int HEIGHT = WIDTH/16*9;
-	public static final int SCALE = 3;
+	public static final int SCALE = 5;
 	public static final String NAME = "Game";
 	
 	private JFrame frame;
