e86315631602ab67b717ec3bf97b7cc3a3a844a6
JustinCSnider <JCSnider100@gmail.com>
2018-08-11 15:42:30 -0700

    paint

--- src/colorRunner/Program.java
+++ src/colorRunner/Program.java
@@ -1,5 +1,23 @@
 package colorRunner;
 
-public class Program {
+import java.awt.Graphics;
 
+import javax.swing.JFrame;
+
+public class Program {
+	
+	Graphics g;
+	
+	public Program() {
+		JFrame game = new JFrame("Color Runner");
+		game.setLayout(null);
+		game.setBounds(400,400,1024,576);
+		game.setVisible(true);
+		g = game.getGraphics();
+		game.paint(g);
+	}
+	
+	public static void main(String[]args) {
+		Program d = new Program();
+	}
 }
